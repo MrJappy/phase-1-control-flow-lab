ad723465e6f766ec856bd9052a18f30722fefc03
+ <+>
2023-01-28 17:05:31 -0500

    commit tree e9e538725691132dcb5abe21639224d254eda65f

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(num){
+  let num;
+  if  (num < 400) {
+    return 'This one is on me!'
+  } else if (num >= 400 && num < 2000) {
+    return 'That will be twenty bucks.'
+  }
+  
 }
 
 function ternaryCheckCity(){
